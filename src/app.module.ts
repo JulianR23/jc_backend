814958bccd0f5fd1837d47a16ccd5175ea4781b1
Julián Clavijo <130173701+JulianR23@users.noreply.github.com>
2026-05-02 18:36:44 -0500

    feat(app): update bootstrap configuration and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ClientsModule } from './clients/clients.module';
+import { CoreModule } from './core/core.module';
+import { LocationsModule } from './location/locations.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { SharedModule } from './shared/shared.module';
+import { ShipmentsModule } from './shipments/shipments.module';
+import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
+    }),
+    PrismaModule,
+    CoreModule,
+    SharedModule,
+    UsersModule,
+    AuthModule,
+    ClientsModule,
+    ProductsModule,
+    LocationsModule,
+    ShipmentsModule,
+  ],
 })
 export class AppModule {}
